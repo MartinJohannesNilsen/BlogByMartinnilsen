48ce1640148d2153c9544767c830bf333cdabf08
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2024-01-22 15:28:19 +0100

    ImageBlurhash api, icons and apidoc color fix

---
+++ pages/api/editorjs/imageblurhash.ts
@@ -0,0 +1,75 @@
+import { blurhashFromURL } from "blurhash-from-url";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { validateAuthAPIToken } from "..";
+
+async function getBlurhash(url: string) {
+	// Default 32x32 size
+	// const output = await blurhashFromURL(url);
+	// Set size
+	const output = await blurhashFromURL(url, {
+		size: 64,
+	});
+	return output;
+}
+
+//  *             example:
+//  *              encoded: "UCBnNqOEACxGNgn%Sday1GwcbHNt}YbIr]W;",
+//  *              width: 1200,
+//  *              height: 700,
+
+/**
+ * @swagger
+ * /api/editorjs/imageblurhash:
+ *   get:
+ *     summary: Generate blurhash
+ *     description: Generate blurhash and return encoded string, in addition to heigth and width of image.
+ *     tags:
+ *       - EditorJS
+ *     parameters:
+ *       - in: query
+ *         name: url
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The URL to encode using the blurhash algorithm.
+ *     responses:
+ *       '200':
+ *         description: Successful response.
+ *         content:
+ *           application/json:
+ *             example:
+ *              encoded: "UCBnNqOEACxGNgn%Sday1GwcbHNt}YbIr]W;"
+ *              width: 1200
+ *              height: 700
+ *
+ *       '400':
+ *         description: Bad request. URL parameter is missing or invalid.
+ *       '500':
+ *         description: Internal server error.
+ *       '501':
+ *         description: Method not supported.
+ */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	// Validate authorized access based on header field 'apikey'
+	const authValidation = validateAuthAPIToken(req);
+	if (!authValidation.isValid) {
+		return res.status(authValidation.code).json({ code: authValidation.code, reason: authValidation.reason });
+	}
+
+	if (!req.query.url) {
+		return res.status(400).json({ code: 400, reason: "Missing url" });
+	}
+
+	if (req.method === "GET") {
+		try {
+			// Get blurhash
+			getBlurhash(req.query.url as string).then((output) => {
+				return res.status(200).json(output);
+			});
+		} catch (err) {
+			return res.status(500).json({ code: 500, reason: err });
+		}
+	} else {
+		return res.status(501).json({ code: 501, reason: "Method not supported" });
+	}
+}
--- pages/apidoc/index.tsx
+++ pages/apidoc/index.tsx
@@ -1,40 +1,44 @@
-import ErrorPage from "next/error";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import "swagger-ui-react/swagger-ui.css";
-import { createdApiDocSpec } from "../../lib/swagger";
-import useAuthorized from "../../components/AuthorizationHook/useAuthorized";
+import ErrorPage from "next/error";
+import { useEffect } from "react";
 import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+import useAuthorized from "../../components/AuthorizationHook/useAuthorized";
+import { createdApiDocSpec } from "../../lib/swagger";
+import { useTheme } from "../../styles/themes/ThemeProvider";
+import { ThemeEnum } from "../../styles/themes/themeMap";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const spec: Record<string, any> = createdApiDocSpec;
-  return {
-    props: {
-      spec,
-    },
-  };
+	const spec: Record<string, any> = createdApiDocSpec;
+	return {
+		props: {
+			spec,
+		},
+	};
 };
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { isAuthorized, status } =
-    process.env.NEXT_PUBLIC_LOCALHOST === "true"
-      ? {
-          isAuthorized: true,
-          status: "authenticated",
-        }
-      : useAuthorized(true);
+	const { setTheme } = useTheme();
 
-  if (status === "authenticated" && !isAuthorized) {
-    return (
-      <ErrorPage
-        statusCode={403}
-        title="You've taken the wrong path! Please return home"
-      />
-    );
-  }
-  if (status === "loading") {
-    return <></>;
-  } else {
-    return <SwaggerUI spec={spec} persistAuthorization={true} />;
-  }
+	useEffect(() => {
+		setTheme(ThemeEnum.Light);
+	}, []);
+
+	const { isAuthorized, status } =
+		process.env.NEXT_PUBLIC_LOCALHOST === "true"
+			? {
+					isAuthorized: true,
+					status: "authenticated",
+			  }
+			: useAuthorized(true);
+
+	if (status === "authenticated" && !isAuthorized) {
+		return <ErrorPage statusCode={403} title="You've taken the wrong path! Please return home" />;
+	}
+	if (status === "loading") {
+		return <></>;
+	} else {
+		return <SwaggerUI spec={spec} persistAuthorization={true} />;
+	}
 }
 export default ApiDoc;
