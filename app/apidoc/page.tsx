78a7d4558eeb6da911a4c36a6186f89c726da17f
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2024-05-03 22:17:47 +0200

    Transition to app router, part 6

---
+++ app/apidoc/page.tsx
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth";
+import { createdApiDocSpec } from "../../lib/swagger";
+import ApiDoc from "./clientPage";
+import { Metadata } from "next";
+
+export async function generateMetadata({ params, searchParams }) {
+	const metadata: Metadata = {
+		title: "APIDoc",
+	};
+	return metadata;
+}
+
+export default async function Page() {
+	// Check authentication
+	const session = await getServerSession();
+	const isAuthorized = process.env.NEXT_PUBLIC_LOCALHOST === "true" || session?.user?.email === process.env.ADMIN_EMAIL;
+
+	// Generate spec
+	const spec: Record<string, any> = createdApiDocSpec;
+
+	return <ApiDoc spec={spec} isAuthorized={isAuthorized} />;
+}
