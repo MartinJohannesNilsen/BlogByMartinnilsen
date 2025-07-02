e0177e9c87562a392def3e66df72d1193863632c
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2025-07-02 17:16:49 +0200

    bump version to 3.2.0

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -1,61 +1,61 @@
 import { createSwaggerSpec } from "next-swagger-doc";
 
 export const apiDocSpec = createSwaggerSpec({
-	apiFolder: "app/api",
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "MJNTech Blog API",
-			version: "3.1.0",
-			// contact: {
-			//   name: "Martin Johannes Nilsen",
-			//   url: "https://links.martinjohannesnilsen.no/",
-			//   // email: "martinjnilsen@icloud.com",
-			// },
-			contact: {
-				name: "Martin Johannes Nilsen",
-				url: process.env.NEXT_PUBLIC_WEBSITE_URL,
-			},
-		},
-		components: {
-			securitySchemes: {
-				ApiKeyAuth: {
-					type: "apiKey",
-					in: "header", // can be "header", "query" or "cookie"
-					name: "apikey", // name of the header, query parameter or cookie
-				},
-			},
-		},
-		security: [
-			{
-				ApiKeyAuth: [],
-			},
-		],
-		tags: [
-			{
-				name: "Default",
-				description: "Default endpoints",
-			},
-			{
-				name: "Media",
-				description: "Endpoints related to media management",
-			},
-			{
-				name: "Notifications",
-				description: "Endpoints related to notifications",
-			},
-			{
-				name: "Posts",
-				description: "Endpoints related to post management",
-			},
-			{
-				name: "Tags",
-				description: "Endpoints related to available tags",
-			},
-			{
-				name: "Views",
-				description: "Endpoints related to view counts",
-			},
-		],
-	},
+  apiFolder: "app/api",
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "MJNTech Blog API",
+      version: "3.2.0",
+      // contact: {
+      //   name: "Martin Johannes Nilsen",
+      //   url: "https://links.martinjohannesnilsen.no/",
+      //   // email: "martinjnilsen@icloud.com",
+      // },
+      contact: {
+        name: "Martin Johannes Nilsen",
+        url: process.env.NEXT_PUBLIC_WEBSITE_URL,
+      },
+    },
+    components: {
+      securitySchemes: {
+        ApiKeyAuth: {
+          type: "apiKey",
+          in: "header", // can be "header", "query" or "cookie"
+          name: "apikey", // name of the header, query parameter or cookie
+        },
+      },
+    },
+    security: [
+      {
+        ApiKeyAuth: [],
+      },
+    ],
+    tags: [
+      {
+        name: "Default",
+        description: "Default endpoints",
+      },
+      {
+        name: "Media",
+        description: "Endpoints related to media management",
+      },
+      {
+        name: "Notifications",
+        description: "Endpoints related to notifications",
+      },
+      {
+        name: "Posts",
+        description: "Endpoints related to post management",
+      },
+      {
+        name: "Tags",
+        description: "Endpoints related to available tags",
+      },
+      {
+        name: "Views",
+        description: "Endpoints related to view counts",
+      },
+    ],
+  },
 });
